aafed9e64e3dadc6dd4adb602366b52c77b9eaf6
Intellcoder <chimeziejoshuac124@gmail.com>
2025-09-19 11:07:42 +0100

    fixed build errors

--- backend/src/utils/imageKit.ts
+++ backend/src/utils/imageKit.ts
@@ -1,9 +1,14 @@
 import ImageKit from "imagekit";
 
 const imagekit = new ImageKit({
-  publicKey: process.env.IMAGEKIT_PUBLIC_KEY || "",
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY || "",
-  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT || "",
+  publicKey:
+    process.env.IMAGEKIT_PUBLIC_KEY || "public_jEy83ckli3iaxhZ2v3C9wKSH9ys=",
+  privateKey:
+    (process.env.IMAGEKIT_PRIVATE_KEY as string) ||
+    "private_TvXumW89vEENTpca6UXfiesNp14=",
+  urlEndpoint:
+    (process.env.IMAGEKIT_URL_ENDPOINT as string) ||
+    "https://ik.imagekit.io/kit1692fz/",
 });
 
 export default imagekit;
