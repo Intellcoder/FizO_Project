24fc711bbbfc0011f1ab8c9caa48ea238da924d7
Intellcoder <chimeziejoshuac124@gmail.com>
2025-09-20 01:35:36 +0100

    validation middleware

--- backend/src/middlewares/validateRequest.ts
+++ backend/src/middlewares/validateRequest.ts
@@ -8,17 +8,21 @@ export function sanitizeRequest(
 ) {
   try {
     //convert all string fields to lowercase
+    if (req.body && typeof req.body === "object") {
+    }
     Object.keys(req.body).forEach((key) => {
       if (typeof req.body[key] === "string") {
         req.body[key] = req.body[key].trim().toLowerCase();
       }
     });
 
-    Object.keys(req.query).forEach((key) => {
-      if (typeof req.query[key] === "string") {
-        req.query[key] = (req.query[key] as string).trim().toLowerCase();
-      }
-    });
+    if (req.query && typeof req.query === "object") {
+      Object.keys(req.query).forEach((key) => {
+        if (typeof req.query[key] === "string") {
+          req.query[key] = (req.query[key] as string).trim().toLowerCase();
+        }
+      });
+    }
 
     next();
   } catch (error) {
